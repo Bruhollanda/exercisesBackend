cb97c559ca388afc95aaf489565926c8077be81b
bruholanda <brunaholandacunha7@gmail.com>
2022-06-19 14:09:01 -0300

    Exercise 4; Selecting a collum from a table.

--- bloco-20-introducao-a-sql/dia-1-banco-de-dados-sql/createTable.sql
+++ bloco-20-introducao-a-sql/dia-1-banco-de-dados-sql/createTable.sql
@@ -1,3 +1,13 @@
+/*
+Nome da tabela: filme
+Colunas:
+
+filme_id - primary key, tipo int, incrementa por 1 cada vez que um valor é inserido automaticamente;
+descricao - não permite nulos, tipo texto (varchar(100));
+ano_lancamento - não permite nulos, tipo int;
+nota - permite nulos, tipo int;
+*/
+
 CREATE TABLE sakila.filme (
   filme_id INT,
   descricao VARCHAR(100) NOT NULL,
@@ -5,3 +15,9 @@ CREATE TABLE sakila.filme (
   nota INT,
   PRIMARY KEY (filme_id));
   
+
+  /*
+  Analise a tabela city e encontre a tabela à qual a coluna country_id faz referência.
+  */
+
+  SELECT country_id FROM sakila.city;
