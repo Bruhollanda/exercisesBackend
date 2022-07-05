afe995e713bbc07cd0fb2a49ef162f7d271f9ef7
bruholanda <brunaholandacunha7@gmail.com>
2022-07-05 18:22:23 -0300

    Creating a simple script

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-node-js/hello-world/index.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-node-js/hello-world/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome? '); // interpreta a resposta como uma string comum
+const age = readline.questionInt('Qual sua idade? '); // converte a resposta para número utilizando o parseInt e retorna um erro caso a pessoa tente inserir algo que não seja um número válido.
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
