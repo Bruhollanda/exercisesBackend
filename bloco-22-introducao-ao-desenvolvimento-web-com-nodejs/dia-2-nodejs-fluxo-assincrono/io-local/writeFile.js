16aacfed570e608f57fb6806485e8a958b1dd69e
bruholanda <brunaholandacunha7@gmail.com>
2022-07-07 12:20:49 -0300

    Writing information in a file;

---
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+fs.writeFile('./meu-arquivo.txt', 'Meu textão')
+  /*
+    Recebe como parâmetro o arquivo a ser editado
+    e o texto que será colocado no arquivo
+    Substitui qualquer informação que havia no arquivo anteriormente.
+  */
+  .then(() => {
+    console.log('Arquivo escrito com sucesso!');
+  })
+  .catch((err) => {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  });
