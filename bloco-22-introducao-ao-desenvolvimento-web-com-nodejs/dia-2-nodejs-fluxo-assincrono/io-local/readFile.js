043ab7b2c88cf53ba360a27dae88d16702819c05
bruholanda <brunaholandacunha7@gmail.com>
2022-07-07 12:13:54 -0300

    readFile using Promises;

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/readFile.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/readFile.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/* const fs = require('fs');
 
 const nomeDoArquivo = 'meu-arquivo.txt';
 
@@ -9,10 +9,31 @@ fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
       - Um parâmetro opcional que, quando é uma string,
       define o encoding que será utilizado durante a leitura do arquivo;
       - Uma callback que permite receber e manipular os dados lidos do arquivo.
-   */
+   
   if (err) {
     console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
     process.exit(1);
   }
   console.log(`Conteúdo do arquivo: ${data}`);
 });
+*/
+
+/* O módulo fs possui um segundo modelo de API que,
+  em vez de trabalhar com callbacks, retorna Promises.
+  Para utilizar a interface de Promises do fs, precisamos
+  alterar a importação do módulo fs para ('fs').promises.
+*/
+
+const fs = require('fs').promises;
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1);
+    // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
+  });
