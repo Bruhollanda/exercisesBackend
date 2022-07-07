f6ca1bc7b8972b6daaba5b856c893dfe17090ac8
bruholanda <brunaholandacunha7@gmail.com>
2022-07-07 12:09:08 -0300

    using fs.readFile;

---
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/readFile.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
+  /*
+    Ele recebe três parâmetros:
+      - O nome do arquivo;
+      - Um parâmetro opcional que, quando é uma string,
+      define o encoding que será utilizado durante a leitura do arquivo;
+      - Uma callback que permite receber e manipular os dados lidos do arquivo.
+   */
+  if (err) {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1);
+  }
+  console.log(`Conteúdo do arquivo: ${data}`);
+});
