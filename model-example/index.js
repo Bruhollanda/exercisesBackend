45b7b1d3c8d7e627b29104b2e1dfa949aa515fbc
bruholanda <brunaholandacunha7@gmail.com>
2022-08-02 10:24:03 -0300

    u

--- model-example/index.js
+++ model-example/index.js
@@ -1,10 +1,11 @@
 // index.js
 
-// const express = require('express');
+const express = require('express');
 
 const Author = require('./models/Author');
+const Books = require('./models/Books');
 
-// const app = express();
+const app = express();
 
 app.get('/authors', async (_req, res) => {
 	const authors = await Author.getAll();
@@ -12,8 +13,28 @@ app.get('/authors', async (_req, res) => {
 	res.status(200).json(authors);
 });
 
-// const PORT = process.env.PORT || 3000;
+app.get('/books', async (req, res) => {
+  const { author_id } = req.query; // desestruturando da query
 
-// app.listen(PORT, () => {
-// 	console.log(`Ouvindo a porta ${PORT}`);
-// });
+	const books = (author_id)
+  ? await Books.getAuthorId(author_id) // se o id foi informado, manda o retorno da função getAuthorId
+  : await Books.getAll(); // senão retorna todos com a função getAll
+
+	res.status(200).json(books);
+});
+
+app.get('/authors/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const author = await Author.findById(id);
+
+	if (!author) return res.status(404).json({ message: 'Not found' });
+
+	res.status(200).json(author);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Ouvindo a porta ${PORT}`);
+});
